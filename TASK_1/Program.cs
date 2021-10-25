a674367841059d6f020acfe94b4fa17dee8b7620
Gokul-1995 <92993326+Gokul-1995@users.noreply.github.com>
2021-10-25 17:08:35 +0530

    Add files via upload

---
+++ TASK_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK_1
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("GOKULAKRISHNAN R");
+            Console.WriteLine("D-86/87, NEW HOUSING UNIT");
+            Console.WriteLine("MANNARGUDI");
+            Console.WriteLine("THIRUVARUR DT"); 
+            Console.WriteLine("614001");
+            Console.WriteLine("8807718814");
+        }
+
+    }
+}
